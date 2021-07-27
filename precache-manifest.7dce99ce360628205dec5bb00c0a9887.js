f6ee52d73846147ae5cd6f4fe55cedfdd140ba6b
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-07-27 12:27:25 +0300

    Updates

---
+++ precache-manifest.7dce99ce360628205dec5bb00c0a9887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f484acaf45907942ef7885ece5c1b462",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8208115378bd3988601b",
+    "url": "./static/css/main.3cb7f518.chunk.css"
+  },
+  {
+    "revision": "48ec29882e4f06fa756d",
+    "url": "./static/js/2.4187ca46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4187ca46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8208115378bd3988601b",
+    "url": "./static/js/main.964c995d.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
